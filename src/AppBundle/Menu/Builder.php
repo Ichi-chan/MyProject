2af6c4b2a010885870b5a1933649fb959c0919de
Lagutenkov Vladimir <exorcist1@mail.ru>
2016-06-30 13:43:57 +0200

    add Menu, fix

--- src/AppBundle/Menu/Builder.php
+++ src/AppBundle/Menu/Builder.php
@@ -2,8 +2,27 @@
 
 namespace AppBundle\Menu;
 
+use Knp\Menu\FactoryInterface;
 use Symfony\Component\DependencyInjection\ContainerAwareInterface;
 use Symfony\Component\DependencyInjection\ContainerAwareTrait;
-use Symfony\Component\DependencyInjection\ContainerInterface;
 
 
+class Builder implements  ContainerAwareInterface
+{
+    use ContainerAwareTrait;
+
+    /**
+     * @param FactoryInterface $factory
+     */
+    public function mainMenu(FactoryInterface $factory)
+    {
+        $menu = $factory->createItem('root');
+        $menu->setChildrenAttribute('class', 'nav navbar-nav');
+        $menu->addChild('Today', array('route' => 'film_list'));
+        $menu->addChild('Soon', array('route' => 'film_soon_list'));
+        $menu->addChild('Archive', array('route' => 'film_archive_list'));
+        $menu->addChild('About', array('route' => 'about_list'));
+        $menu->addChild('AddFilm', array('route' => 'films_append'));
+        return $menu;
+    }
+}

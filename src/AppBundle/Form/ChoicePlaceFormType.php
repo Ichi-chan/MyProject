f36266db31c1a0aa5a9df79b771badd67f23e5f7
Lagutenkov Vladimir <exorcist1@mail.ru>
2016-08-04 11:24:18 +0000

    some changes

--- src/AppBundle/Form/ChoicePlaceFormType.php
+++ src/AppBundle/Form/ChoicePlaceFormType.php
@@ -3,74 +3,58 @@
 namespace AppBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\ButtonType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
-
 
 class ChoicePlaceFormType extends AbstractType
 {
-   // private $manager;
+    private $manager;
 
     public function __construct()
     {
     }
-
+    
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         //for ($i=0;$i<100;$i++)
 
-        $qqq = $builder->getData();
-        if ($qqq != null) {
-            $qqq->getNumPlace();
-            $place = $qqq->getNumPlace();
+        /** @var Place $place */
+       /* $availability = $builder->getData();
+        if ($availability != null) {
+        //    $availability->getNumPlace();
+            $place = $availability;*/
+        $opt=$options['places'];
+        $availability = $builder->getData();
+        $availability=trim($availability);
+        if ($availability != null) {
+//$availability->getNumPlace();
+            $place = $availability;
 
         } else {
             $place = ',';
         }
 
-       // $builder;
-        //$label = "";
+        $attr = array('class' => 'btn btn-primary place-button',
+            'background' => ' #2aabd2',
+            'style' => 'width: 100%; height: 100%',);
         for ($i = 1; $i < 11; $i++) {
             for ($j = 1; $j < 11; $j++) {
                 $label = ($i) . "-" . ($j);
                 if (strripos($place, $label) == true) {
-                    $builder->add(
-                        '[' . $i . ']-[' . $j . ']',
-                        ButtonType::class,
-                        array(
-                            'label' => $label,
-                            "attr" => array(
-                                'class' => 'btn btn-primary',
-                                'background' => ' #868686',
-                                'display'=>"block",
-                                'border-radius' => '5px',
-                                'box-shadow' => '0px 1px 3px',
-                                'font-size' => '30px',
-                                'float-displace' => 'ident',
-                                'disabled' => 'disabled',
-                                'style'=>'width: 100%; height: 100%',
-                            )
-                        )
-                    );
+                    $attr['disabled'] = true;
                 } else {
-                    $builder->add(
-                        '[' . $i . ']-[' . $j . ']',
-                        ButtonType::class,
-                        array(
-                            'label' => $label,
-                            "attr" => array(
-                                'display'=>"block",
-                                'class' => 'btn btn-primary',
-                                'background' => ' #2aabd2',
-                                'border-radius' => '5px',
-                                'box-shadow' => '0px 1px 3px',
-                                'font-size' => '30px',
-                                'float-displace' => 'ident',
-                                'style'=>'width: 100%; height: 100%',
-                            )
-                        )
-                    );
+                    $attr['disabled'] = false;
                 }
+                $builder->add(
+                    '[' . $i . ']-[' . $j . ']',
+                    ButtonType::class,
+                    array(
+                        'label' => $label,
+                        "attr" => $attr
+                    )
+                );
+
             }
         }
         $builder->add(
@@ -82,10 +66,17 @@ class ChoicePlaceFormType extends AbstractType
                     'data-toggle' => "modal",
                     'data-target' => "#myModal",
                     'name' => "Reserved",
-                    'class' => "btn btn-primary btn-lg",
-                    'style'=>'margin-left:75%',
+                    'class' => "btn btn-primary btn-lg btn-reserved",
+                    'style' => 'margin-left:75%',
                 )
             )
         );
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'places' => '',
+        ));
     }
 }
